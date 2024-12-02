2a96f994ba0427ee82624766d7593875d43cc5fa
Ziad Ahmed Helmy <ziad.ahmedcs21@fcai.usc.edu.eg>
2024-12-02 22:50:51 +0200

    change the series endpoint

--- lib/controller/Tv_Series/Data/Repo/Series_Repo.dart
+++ lib/controller/Tv_Series/Data/Repo/Series_Repo.dart
@@ -8,7 +8,7 @@ class SeriesRepository {
   Future<List<Series>?> fetchSeries() async {
     try {
       var response = await _dio.get(
-        "${ApiEndPoints.BASE_URL}${ApiEndPoints.Trending_Tv_Shows}?language=en-US&page=1",
+        "${ApiEndPoints.BASE_URL}${ApiEndPoints.Popular_Tv_Shows}?language=en-US&page=1",
         options: Options(
           headers: {
             "accept": "application/json",
@@ -31,7 +31,7 @@ class SeriesRepository {
    Future<List<Series>> fetchSeriesPagination(int page) async {
     try {
       var response = await _dio.get(
-        "${ApiEndPoints.BASE_URL}${ApiEndPoints.Trending_Tv_Shows}?language=en-US&page=$page",
+        "${ApiEndPoints.BASE_URL}${ApiEndPoints.Popular_Tv_Shows}?language=en-US&page=$page",
         options: Options(
           headers: {
             "accept": "application/json",
