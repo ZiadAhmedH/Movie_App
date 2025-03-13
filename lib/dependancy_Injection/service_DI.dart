2b2b6f82c7857c4fe92880f56624ff3c507306de
ZiadAhmedH <ziad.ahmedcs21@fcai.usc.edu.eg>
2025-03-13 04:17:07 +0200

    add the new useCase to DI

--- lib/dependancy_Injection/service_DI.dart
+++ lib/dependancy_Injection/service_DI.dart
@@ -1,8 +1,10 @@
 import 'package:get_it/get_it.dart';
+import 'package:movies_app/Movies/Movie/Presentation/controller/movie_details_bloc.dart';
 import '../Movies/Movie/Data/datasource/movie_remote_data_source.dart';
 import '../Movies/Movie/Data/repository/movies_repository.dart';
 import '../Movies/Movie/Presentation/controller/movies_bloc.dart';
 import '../Movies/Movie/domain/repository/base_Movie_Repository.dart';
+import '../Movies/Movie/domain/usecases/fetch_Movie_Details.dart';
 import '../Movies/Movie/domain/usecases/fetch_Playing_Now_Movies.dart';
 import '../Movies/Movie/domain/usecases/fetch_Popular_Movies_Pagination.dart';
 import '../Movies/Movie/domain/usecases/fetch_Top_Rated_Movies.dart';
@@ -17,9 +19,12 @@ void init() {
     // bloc using registerFactory ;> when we want to create a new instance of the bloc every time
     sl.registerFactory(() => MoviesBloc(sl() , sl() , sl()));
 
+    sl.registerFactory(() => MovieDetailsBloc(sl()));
+
     sl.registerLazySingleton(() => FetchPlayingNowMovies(sl()));
     sl.registerLazySingleton(() => FetchPopularMoviesPagination(sl()));
     sl.registerLazySingleton(()=> FetchTopRatedMovies(sl()));
+    sl.registerLazySingleton(()=> FetchMovieDetailsUseCase(sl()));
 
     sl.registerLazySingleton<BaseMovieRepository>(() => MoviesRepository(sl()));
     sl.registerLazySingleton<BaseMovieRemoteDataSource>(() => MovieRemoteDataSource());
